bf986c89b38a952c7e18acc73bd01e6d9575d9bf
aakashloyar <aakashloyar@gmail.com>
2025-02-13 17:36:50 +0530

    configured tailwand for designing

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,12 @@
+import Image, { type ImageProps } from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+
+export default function Home() {
+  return (
+    <div className='text-red-900'>
+      hi there jn
+    </div>
+  );
+}
